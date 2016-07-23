64a9d34ad6c587f111c99b910ef853d1c5cc420f
b-lambert <benjaminhlambert@gmail.com>
2016-07-23 15:04:12 -0700

    initial commit

--- weather app/ViewController.swift
+++ weather app/ViewController.swift
@@ -1,25 +1,42 @@
-//
-//  ViewController.swift
-//  weather app
-//
-//  Created by Ben Lambert on 7/22/16.
-//  Copyright © 2016 blambert. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var numDaysLabel: UILabel!
+    @IBOutlet var numDaysSlider: UISlider!
+    @IBOutlet var cityTextField: UITextField!
+    @IBOutlet var getDataButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getDataButton.enabled = false
+    }
+    @IBAction func endOnExit(sender: AnyObject) {
+        
+    }
+    @IBAction func editingChanged(sender: AnyObject) {
+        getDataButton.enabled = cityTextField.text! != ""
+    }
+    @IBAction func primaryActionTriggered(sender: AnyObject) {
+        print("primary action triggered")
     }
 
+    @IBAction func cityTextFieldEditingDidEnd(sender: AnyObject) {
+        print("text field editing did end")
+        getDataButton.enabled = cityTextField.text != ""
+    }
+    
+    @IBAction func daySliderChanged(sender: AnyObject) {
+        let numDays = Int(numDaysSlider.value)
+        numDaysLabel.text = String(numDays)
+
+    }
+    @IBAction func valueChanged(sender: AnyObject) {
+        print("val changed")
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
