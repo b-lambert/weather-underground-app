fa2724c336153801cef937371c59a6d772ac5d39
b-lambert <benjaminhlambert@gmail.com>
2016-07-24 15:17:58 -0700

    working on autolayout for 2nd screen

--- weather app/ViewController.swift
+++ weather app/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var numDaysLabel: UILabel!
     @IBOutlet var numDaysSlider: UISlider!
     @IBOutlet var cityTextField: UITextField!
@@ -11,27 +12,16 @@ class ViewController: UIViewController {
         getDataButton.enabled = false
     }
     @IBAction func endOnExit(sender: AnyObject) {
-        
-    }
-    @IBAction func editingChanged(sender: AnyObject) {
-        getDataButton.enabled = cityTextField.text! != ""
-    }
-    @IBAction func primaryActionTriggered(sender: AnyObject) {
-        print("primary action triggered")
+
     }
 
-    @IBAction func cityTextFieldEditingDidEnd(sender: AnyObject) {
-        print("text field editing did end")
-        getDataButton.enabled = cityTextField.text != ""
+    @IBAction func editingChanged(sender: AnyObject) {
+        getDataButton.enabled = cityTextField.text! != ""
     }
     
     @IBAction func daySliderChanged(sender: AnyObject) {
         let numDays = Int(numDaysSlider.value)
         numDaysLabel.text = String(numDays)
-
-    }
-    @IBAction func valueChanged(sender: AnyObject) {
-        print("val changed")
     }
  
     override func didReceiveMemoryWarning() {
