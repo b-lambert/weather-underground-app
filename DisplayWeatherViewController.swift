3b79fe5578e1842090110c736ecb304ffe06aa2d
b-lambert <benjaminhlambert@gmail.com>
2016-07-24 16:09:50 -0700

    added a ton of outlets

--- DisplayWeatherViewController.swift
+++ DisplayWeatherViewController.swift
@@ -2,7 +2,20 @@ import UIKit
 
 class DisplayWeatherViewController: UIViewController {
     
+    @IBOutlet var highTempLabel: UILabel!
+    @IBOutlet var lowTempLabel: UILabel!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var selectDaySegmentedControl: UISegmentedControl!
+    @IBOutlet var weatherIcon: UIImageView!
+    @IBOutlet var weatherStatusLabel: UILabel!
+
     var numDays: Int = 1
+    // TODO make this private somehow
+    var currentDayIndex: Int = 0
+
+    @IBAction func selectDayValueChanged(sender: AnyObject) {
+        currentDayIndex = selectDaySegmentedControl.selectedSegmentIndex
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
